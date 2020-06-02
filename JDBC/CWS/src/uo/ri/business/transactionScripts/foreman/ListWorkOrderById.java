3dd752d1467b1f60012e6d3dc93d7d5c9f03fe94
UO258935 <UO258935@uniovi.es>
2020-06-02 13:20:02 +0200

    Workorder - ListWorkOrderById -> comprobado ID

--- JDBC/CWS/src/uo/ri/business/transactionScripts/foreman/ListWorkOrderById.java
+++ JDBC/CWS/src/uo/ri/business/transactionScripts/foreman/ListWorkOrderById.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 import alb.util.jdbc.Jdbc;
 import uo.ri.business.dto.WorkOrderDto;
+import uo.ri.business.exception.BusinessException;
 import uo.ri.conf.PersistenceFactory;
 import uo.ri.persistance.WorkOrderGateway;
 
@@ -15,10 +16,15 @@ public class ListWorkOrderById {
 		this.id = id;
 	}
 
-	public WorkOrderDto execute() {
+	public WorkOrderDto execute() throws BusinessException {
 		try (Connection c = Jdbc.createThreadConnection()) {
 			WorkOrderGateway wog = PersistenceFactory.getWorkOrderGateway();
 			wog.setConnection(c);
+			//Comprobar que el id exista
+			if (wog.findWorkOrderById(id) == null) {
+				c.rollback();
+				throw new BusinessException("the work orders does not exist");
+			}
 			return wog.findWorkOrderById(id);
 		} catch (SQLException e) {
 			throw new RuntimeException("Error de conexion");
