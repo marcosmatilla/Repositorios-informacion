71266b248519db7af019aad41eaa48b780134e15
UO258935 <UO258935@uniovi.es>
2020-06-04 18:44:07 +0200

    Course attendance service

--- JDBC/CWS/src/uo/ri/business/serviceLayer/administrator/training/courseattendance/impl/CourseAttendanceServiceImpl.java
+++ JDBC/CWS/src/uo/ri/business/serviceLayer/administrator/training/courseattendance/impl/CourseAttendanceServiceImpl.java
@@ -7,6 +7,10 @@ import uo.ri.business.dto.EnrollmentDto;
 import uo.ri.business.dto.MechanicDto;
 import uo.ri.business.exception.BusinessException;
 import uo.ri.business.serviceLayer.administrator.training.courseattendance.CourseAttendanceService;
+import uo.ri.business.transactionScripts.administrator.mechanic.ListMechanic;
+import uo.ri.business.transactionScripts.administrator.training.course.ListAllCourses;
+import uo.ri.business.transactionScripts.administrator.training.courseattendance.DeleteCourseAttendance;
+import uo.ri.business.transactionScripts.administrator.training.courseattendance.ListAttendanceByCourseId;
 import uo.ri.business.transactionScripts.administrator.training.courseattendance.RegisterCourseAttendance;
 
 public class CourseAttendanceServiceImpl implements CourseAttendanceService {
@@ -19,26 +23,26 @@ public class CourseAttendanceServiceImpl implements CourseAttendanceService {
 
 	@Override
 	public void deleteAttendace(Long id) throws BusinessException {
-		// TODO Auto-generated method stub
-
+		DeleteCourseAttendance da = new DeleteCourseAttendance(id);
+		da.execute();
 	}
 
 	@Override
 	public List<EnrollmentDto> findAttendanceByCourseId(Long id) throws BusinessException {
-		// TODO Auto-generated method stub
-		return null;
+		ListAttendanceByCourseId labci = new ListAttendanceByCourseId(id);
+		return labci.execute();
 	}
 
 	@Override
 	public List<CourseDto> findAllActiveCourses() throws BusinessException {
-		// TODO Auto-generated method stub
-		return null;
+		ListAllCourses lac = new ListAllCourses();
+		return lac.execute();
 	}
 
 	@Override
 	public List<MechanicDto> findAllActiveMechanics() throws BusinessException {
-		// TODO Auto-generated method stub
-		return null;
+		ListMechanic lm = new ListMechanic();
+		return lm.execute();
 	}
 
 }
